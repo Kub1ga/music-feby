4380ae214601534d2df7b23e92cf21ed0e6d9b49
Kub1ga <99202219+Kub1ga@users.noreply.github.com>
2022-03-16 16:57:59 +0700

    Add files via upload

---
+++ music/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+    {
+        name: "(Put On Your) Dancing Pants",
+        artist: "Reed Mathis",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Benji",
+        artist: "Dyalla",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Burbank Late Nights",
+        artist: "Squadda B",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Oh Fire",
+        artist: "Carmen Maria and Edu Espinal",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Then A Left Turn",
+        artist: "Jeremy Korpas",
+        img: "music-5",
+        src: "music-6"
+    },
+
+];
